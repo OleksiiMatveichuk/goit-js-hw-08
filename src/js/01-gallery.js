04c74bf62b4d9f2532d9638b263ce5875ef856ee
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-05-01 17:36:38 +0300

    add 1 work

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+
+const arrItems = galleryItems.map(el => {
+  const item = `<li class="gallery__item">
+  <a class="gallery__link" href="${el.original}">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      alt="${el.description}"
+      title="${el.description}"
+    />
+  </a>
+</li>`;
+  return item;
+});
+
+const list = document.querySelector('.gallery');
+list.innerHTML = arrItems.join('');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
