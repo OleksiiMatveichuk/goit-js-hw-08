970e8ffac49271a226f34549ce0e81975f8d983d
OleksiiMatveichuk <126767121+OleksiiMatveichuk@users.noreply.github.com>
2023-05-03 16:42:53 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,18 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Vimeo(iframe);
+const localKey = 'videoplayer-current-time';
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
-const onPlay = function (data) {
-  // data is an object containing properties specific to that event
+const onPlay = data => {
+  const playTime = data.seconds;
+  localStorage.setItem(localKey, JSON.stringify(playTime));
 };
 
-player.on('play', onPlay);
+const timeDef = () => {
+  player.setCurrentTime(localStorage.getItem(localKey));
+};
+
+player.on('timeupdate', throttle(onPlay, 500));
+player.off('play', onPlay);
+timeDef();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('[name="email"]');
+const messageEl = document.querySelector('[name="message"]');
+const btnSubmit = document.querySelector('[type="submit"]');
+const LocalKey = 'feedback-form-state';
 
+const submitForm = event => {
+  event.preventDefault();
+  console.log({
+    email: emailEl.value,
+    message: messageEl.value,
+  });
+  localStorage.removeItem(LocalKey);
+  event.currentTarget.reset();
+};
+
+const inputForm = evt => {
+  const objct = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(LocalKey, JSON.stringify(objct));
+};
+
+const defForm = () => {
+  const locolData = localStorage.getItem(LocalKey);
+  if (!locolData) {
+    return;
+  }
+  const { email, message } = JSON.parse(locolData);
+
+  form.elements.email.value = email;
+  form.elements.message.value = message;
+};
+
+form.addEventListener('submit', submitForm);
+form.addEventListener('input', throttle(inputForm, 500));
+defForm();
